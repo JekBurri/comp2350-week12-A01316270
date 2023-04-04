ed5a953d92e94d2f29bbb814f4e01a9d588902b4
Jack Boeri <53635108+JekBurri@users.noreply.github.com>
2023-04-04 16:27:02 -0700

    changed conn url

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,8 +1,9 @@
 const database = require("mongoose");
 const is_qoddi = process.env.IS_QODDI || false;
 const databaseName = "lab_example"
-const qoddiURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.szw2cob.mongodb.net/"+databaseName+"/?retryWrites=true&w=majority"
+const qoddiURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.szw2cob.mongodb.net/"+databaseName+"?retryWrites=true&w=majority"
                // mongodb+srv://theMongoAdmin:<password>          @cluster0.szw2cob.mongodb.net/                ?retryWrites=true&w=majority
+            //    mongodb+srv://theMongoAdmin:<password>@cluster0.szw2cob.mongodb.net/?retryWrites=true&w=majority
 const localURI = "mongodb://127.0.0.1/"+databaseName+"?authSource=admin&retryWrites=true"
 if (is_qoddi) {
     database.connect(qoddiURI, {useNewUrlParser: true, useUnifiedTopology: true});
